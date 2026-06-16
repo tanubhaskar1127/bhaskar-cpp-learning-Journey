15d17012a9125dd12d35f982fb0e6ede3a9d02a5
Tanubhaskar <tanubhaskar1127@gmail.com>
2026-06-16 20:35:05 +0530

    Initial C++ learning journey

---
+++ Day01_HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, CPP Journey!";
+    return 0;
+}
---
+++ Day02_Variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name = "Millionaire";
+    int age = 25;
+    double height = 5.9;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+
+    return 0;
+}
