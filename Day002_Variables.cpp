0301e35d4a3f0fbdc3a7d1754e330b939a19a3f0
Tanubhaskar <tanubhaskar1127@gmail.com>
2026-06-17 18:23:47 +0530

    Completed Day02 Variables and Data Types

---
+++ Day002_Variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name = "Millionaire";
+    int age = 25;
+    float height = 5.9;
+    char grade = 'A';
+    bool isStudent = true;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student Status: " << isStudent << endl;
+
+    return 0;
+}
